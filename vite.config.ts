26fc044b6dde2dc3aaf223875eec6bf6a94c213d
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-22 15:59:28 -0700

    fix: build config (#24)
    * fix: package name
    
    * fix: emit TS declarations in the bundle
    
    * fix: move React under peerDependencies
    
    * fix: exclude React from the bundle
    
    * fix: pin vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+import { peerDependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Output .d.ts files.
+    // The `rollupTypes` option merges all declarations into a single file.
+    dts({ rollupTypes: true }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
       formats: ['es'],
+    },
+    rollupOptions: {
+      // Exclude peer dependencies from the bundle to reduce bundle size
+      external: Object.keys(peerDependencies),
     },
   },
 })
