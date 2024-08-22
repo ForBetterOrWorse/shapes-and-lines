de71f57a53bd46c2eddda41d1c61c84591071fd0
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-23 04:56:54 +0700

    fix: emit TS declarations in the bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }), // Output .d.ts files
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
       formats: ['es'],
     },
   },
