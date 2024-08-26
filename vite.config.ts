37937db3e9e4f00ce296f4589490540fdd398ce4
Huyen Nguyen <25715018+huyenltnguyen@users.noreply.github.com>
2024-08-26 13:14:34 -0700

    fix: build config (#40)
    * fix: build config
    
    * chore: add prepublishOnly script
    
    * pin vite-plugin-lib-inject-css version
    
    * add storybook-static to .gitignore
    
    * remove types field

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
 import { peerDependencies } from './package.json'
 
@@ -9,6 +10,7 @@ import { peerDependencies } from './package.json'
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     // Output .d.ts files.
     // The `rollupTypes` option merges all declarations into a single file.
     dts({ rollupTypes: true }),
